c9c6c6190742a9c585c5edeca5d832c31c90cede
madeleineostoja <madi@madeleineostoja.com>
2021-06-01 22:07:04 +1200

    Fix type issue

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,7 +1,7 @@
-import watchMedia from 'svelte-media';
 import { customMedia } from '$src/styles/breakpoints.json';
-import { writable } from 'svelte/store';
 import type { Global } from '$types/_generated/prismic';
+import watchMedia from 'svelte-media';
+import { writable } from 'svelte/store';
 
-export const media = watchMedia(customMedia);
+export const media: any = watchMedia(customMedia);
 export const globalData = writable<Global>({} as Global);
