608e0aa4a1c72167913109389e05df3dd0d2def7
madeleineostoja <madi@madeleineostoja.com>
2021-06-16 17:14:45 +1200

    fix storybook

--- .storybook/manager.cjs
+++ .storybook/manager.cjs
@@ -6,7 +6,7 @@ addons.setConfig({
     base: 'light',
     brandTitle: 'Madeleine Ostoja',
     brandUrl: 'https://madeleineostoja.com',
-    brandImage: '../public/icon.png',
+    brandImage: './icon.png',
     colorPrimary: '#D71673',
     colorSecondary: '#D71673'
   }),
